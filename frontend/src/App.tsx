dad80c1406a030a950e88edc9c2834cb112f80ea
John <johnolshea@gmail.com>
2024-07-25 01:25:56 +0100

    Feat: Create Layout scaffolding with header and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,29 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom"
+import Layout from "./layouts/Layout"
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<span className="text-4xl">Home Page</span>}></Route>
-        <Route path="/search" element={<>Search Page</>}></Route>
-        <Route path="*" element={<Navigate to="/" />}></Route>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   )
