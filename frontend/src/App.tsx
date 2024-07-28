7a9b937d3c58f2d86085db4ac114b818287f039d
John <johnolshea@gmail.com>
2024-07-28 01:28:57 +0100

    Core: Add Register and login. Add e2e test for register and login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,10 @@ import {
   Route,
   Routes,
   Navigate,
-} from "react-router-dom"
-import Layout from "./layouts/Layout"
+} from 'react-router-dom';
+import Layout from './layouts/Layout';
+import Register from './pages/Register';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
@@ -26,10 +28,26 @@ function App() {
             </Layout>
           }
         />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />         
+        <Route
+          path="/sign-in"
+          element={
+            <Layout>
+              <SignIn />
+            </Layout>
+          }
+        />        
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
